0a068553b77b7dc3fb64de26d55f8691ec38238c
KaiqueGSilva <kaka.gon.silva@gmail.com>
2025-03-23 21:35:45 -0300

    Atualizado

--- app/home.tsx
+++ app/home.tsx
@@ -1,13 +1,14 @@
+// app/home.tsx
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className='min-h-screen bg-gradient-to-r from-blue-400 via-indigo-400 to-purple-500 text-white'>
       <nav className='flex justify-center space-x-8 py-4 bg-black/50 sticky top-0'>
-        <Link href='/About' className='hover:underline'>About</Link>
-        <Link href='/Project' className='hover:underline'>Projects</Link>
-        <Link href='/Skill' className='hover:underline'>Skills</Link>
-        <Link href='/Contact' className='hover:underline'>Contact</Link>
+        <Link href='/about' className='hover:underline'>About</Link>
+        <Link href='/projects' className='hover:underline'>Projects</Link>
+        <Link href='/skills' className='hover:underline'>Skills</Link>
+        <Link href='/contact' className='hover:underline'>Contact</Link>
       </nav>
 
       <div className='container mx-auto mt-10 px-4'>
