3e36d4031a465067dd8eaaa2265bc8251e44c152
KaiqueGSilva <kaka.gon.silva@gmail.com>
2025-03-24 18:16:32 -0300

    Atualizado

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <nav className="bg-gray-800 p-4 text-white flex gap-4">
-      <Link href="/">Pagina Inicial</Link>
-      <Link href="/about">Sobre</Link>
-      <Link href="/projects">Projetos</Link>
-      <Link href="/skills">Habilidades</Link>
-      <Link href="/contact">Contato</Link>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/projects">Projects</Link>
+      <Link href="/skills">Skills</Link>
+      <Link href="/contact">Contact</Link>
     </nav>
   );
 }
