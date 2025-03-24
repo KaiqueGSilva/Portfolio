0707ddc680c982fd0aed30f8c85e5d2f1cea0406
KaiqueGSilva <kaka.gon.silva@gmail.com>
2025-03-23 21:27:29 -0300

    Atualizado

--- app/home.tsx
+++ app/home.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className='min-h-screen bg-gradient-to-r from-blue-400 via-indigo-400 to-purple-500 text-white'>
       <nav className='flex justify-center space-x-8 py-4 bg-black/50 sticky top-0'>
-        <Link href='/' className='hover:underline'>Home</Link>
         <Link href='/about' className='hover:underline'>About</Link>
         <Link href='/projects' className='hover:underline'>Projects</Link>
         <Link href='/skills' className='hover:underline'>Skills</Link>
