4cae4695551f167e55703cf5f438bd2d7289c029
KaiqueGSilva <kaka.gon.silva@gmail.com>
2025-03-24 14:44:11 -0300

    Atualizado

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className='flex justify-center space-x-8 py-4 bg-black/50 sticky top-0'>
-        <Link href='/' className='hover:underline'>Home</Link>
-        <Link href='/about' className='hover:underline'>About</Link>
-        <Link href='/projects' className='hover:underline'>Projects</Link>
-        <Link href='/skills' className='hover:underline'>Skills</Link>
-        <Link href='/contact' className='hover:underline'>Contact</Link>
-      </nav>
+    <nav className="bg-gray-800 p-4 text-white flex gap-4">
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/project">Projects</Link>
+      <Link href="/skills">Skills</Link>
+      <Link href="/contact">Contact</Link>
+    </nav>
   );
 }
